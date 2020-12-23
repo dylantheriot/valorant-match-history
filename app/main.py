57e248799507c8bd7151a4d2c93c540679000269
Dylan Theriot <dylantheriot@gmail.com>
2020-12-23 17:10:51 -0600

    undoing change

--- app/main.py
+++ app/main.py
@@ -16,13 +16,6 @@ match_movement_hash = {
   'DEMOTED': 'Demoted'
 }
 
-@app.before_request
-def before_request():
-    if not request.is_secure():
-        url = request.url.replace('http://', 'https://', 1)
-        code = 301
-        return redirect(url, code=code)
-
 @app.route('/')
 def home():
   return render_template('login.html')
