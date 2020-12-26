8eb2c3398325e6dd2d2e2bfde0872d35e4398aca
Dylan Theriot <dylantheriot@gmail.com>
2020-12-26 05:25:32 -0600

    stopping the site

--- app/main.py
+++ app/main.py
@@ -51,94 +51,94 @@ def ratelimit_handler(e):
 
 @app.route('/')
 def home():
-  return render_template('login.html')
+  return render_template('gameover.html')
 
 # Redirect people to login page
 @app.route('/match_history', methods=['GET'])
 def redirect_to_login():
   return redirect(url_for('home'))
 
-@app.route('/match_history', methods=['POST'])
-def display_match_history():
-  username = request.form['username']
-  password = request.form['password']
-  region = request.form['region']
-  try:
-    client_ip = request.headers.getlist('X-Forwarded-For')[0]
-  except:
-    print('unknown IP Address')
-    client_ip = request.remote_addr
+# @app.route('/match_history', methods=['POST'])
+# def display_match_history():
+#   username = request.form['username']
+#   password = request.form['password']
+#   region = request.form['region']
+#   try:
+#     client_ip = request.headers.getlist('X-Forwarded-For')[0]
+#   except:
+#     print('unknown IP Address')
+#     client_ip = request.remote_addr
 
-  print('client ip 1:', request.headers.getlist('X-Forwarded-For'))
+#   print('client ip 1:', request.headers.getlist('X-Forwarded-For'))
     
-  # Attempt login
-  try:
-    valorant = ValorantAPI(username, password, region, client_ip)
-  except:
-    print('A login error occurred. F')
-    return render_template('error.html', error='Invalid username/password or incorrect region.')
+#   # Attempt login
+#   try:
+#     valorant = ValorantAPI(username, password, region, client_ip)
+#   except:
+#     print('A login error occurred. F')
+#     return render_template('error.html', error='Invalid username/password or incorrect region.')
 
-  # Attempt to acquire match history data
-  try:
-    json_res = valorant.get_match_history()
-  except:
-    print('Cannot get match history.')
-    return render_template('error.html', error='Cannot get match history.')
+#   # Attempt to acquire match history data
+#   try:
+#     json_res = valorant.get_match_history()
+#   except:
+#     print('Cannot get match history.')
+#     return render_template('error.html', error='Cannot get match history.')
 
-  # Attempt to parse through match history data
-  try:
-    posts = []
-    for match in json_res['Matches']:
-      # print(match)
-      if match['CompetitiveMovement'] == 'MOVEMENT_UNKNOWN':
-        continue
-      match_movement, game_outcome, main_color, gradient_color = match_movement_hash[match['CompetitiveMovement']]
-      lp_change = ''
+#   # Attempt to parse through match history data
+#   try:
+#     posts = []
+#     for match in json_res['Matches']:
+#       # print(match)
+#       if match['CompetitiveMovement'] == 'MOVEMENT_UNKNOWN':
+#         continue
+#       match_movement, game_outcome, main_color, gradient_color = match_movement_hash[match['CompetitiveMovement']]
+#       lp_change = ''
 
-      game_map = 'images/maps/' + maps_hash[match['MapID']] + '.png'
+#       game_map = 'images/maps/' + maps_hash[match['MapID']] + '.png'
 
-      tier = 'images/ranks/' + str(match['TierAfterUpdate']) + '.png'
+#       tier = 'images/ranks/' + str(match['TierAfterUpdate']) + '.png'
       
-      epoch_time = match['MatchStartTime'] // 1000
-      date = time.strftime('%m-%d-%Y', time.localtime(epoch_time))
+#       epoch_time = match['MatchStartTime'] // 1000
+#       date = time.strftime('%m-%d-%Y', time.localtime(epoch_time))
 
-      before = match['TierProgressBeforeUpdate']
-      after = match['TierProgressAfterUpdate']
+#       before = match['TierProgressBeforeUpdate']
+#       after = match['TierProgressAfterUpdate']
 
-      # calculate lp change, TODO: do this more efficiently
-      if match['CompetitiveMovement'] == 'PROMOTED':
-        lp_change = '+' + str(after + 100 - before)
-      elif match['CompetitiveMovement'] == 'DEMOTED':
-        lp_change = '-' + str(before + 100 - after)
-      else:
-        if before < after:
-          # won
-          lp_change = '+' + str(after - before)
-        else:
-          # lost
-          lp_change = str(after - before)
+#       # calculate lp change, TODO: do this more efficiently
+#       if match['CompetitiveMovement'] == 'PROMOTED':
+#         lp_change = '+' + str(after + 100 - before)
+#       elif match['CompetitiveMovement'] == 'DEMOTED':
+#         lp_change = '-' + str(before + 100 - after)
+#       else:
+#         if before < after:
+#           # won
+#           lp_change = '+' + str(after - before)
+#         else:
+#           # lost
+#           lp_change = str(after - before)
 
-      match_data = {
-        'lp_change': lp_change,
-        'current_lp': after,
-        'game_outcome': game_outcome,
-        'movement': match_movement,
-        'tier': tier,
-        'date': date,
-        'game_map': game_map,        
-        'main_color': main_color,
-        'gradient_color': gradient_color
-      }
+#       match_data = {
+#         'lp_change': lp_change,
+#         'current_lp': after,
+#         'game_outcome': game_outcome,
+#         'movement': match_movement,
+#         'tier': tier,
+#         'date': date,
+#         'game_map': game_map,        
+#         'main_color': main_color,
+#         'gradient_color': gradient_color
+#       }
 
-      posts.append(match_data)
+#       posts.append(match_data)
 
-    print(posts)
-    return render_template('match_history.html', posts=posts, name=valorant.game_name, title='VALORANTELO - Match History')
-  except:
-    print('Error parsing match history data.')
-    temp = json_res
-    print('Match history failed with:', temp)
-    return render_template('error.html', error='Error parsing match history data. An administrator will look into this. Try again soon.')
+#     print(posts)
+#     return render_template('match_history.html', posts=posts, name=valorant.game_name, title='VALORANTELO - Match History')
+#   except:
+#     print('Error parsing match history data.')
+#     temp = json_res
+#     print('Match history failed with:', temp)
+#     return render_template('error.html', error='Error parsing match history data. An administrator will look into this. Try again soon.')
 
 @app.route('/example', methods=['GET'])
 def view_example_data():
